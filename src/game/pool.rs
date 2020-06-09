debd9eb94f805d71e6592f563b3887e87feeece4
Antoine Gersant <antoine.gersant@lesforges.org>
2020-06-08 20:12:45 -0700

    One game per server, not per channel

--- src/game/pool.rs
+++ src/game/pool.rs
@@ -1,6 +1,6 @@
 use anyhow::*;
 use serenity::client::Context as SerenityContext;
-use serenity::model::id::ChannelId;
+use serenity::model::id::{ChannelId, GuildId};
 use serenity::prelude::Mutex;
 use std::collections::HashMap;
 use std::sync::{Arc, RwLock};
@@ -12,14 +12,22 @@ use crate::DiscordOutputManager;
 
 #[derive(Default)]
 pub struct Pool {
-    games: RwLock<HashMap<ChannelId, Arc<Mutex<Game>>>>,
+    games: RwLock<HashMap<GuildId, Arc<Mutex<Game>>>>,
 }
 
 impl Pool {
     pub fn get_game(&self, ctx: &SerenityContext, channel: ChannelId) -> Result<Arc<Mutex<Game>>> {
+        let guild = ctx
+            .cache
+            .read()
+            .guild_channel(channel)
+            .context("Server not found")?
+            .read()
+            .guild_id;
+
         let game_exists = {
             let map = self.games.read().expect("Pool RwLock poisoned");
-            map.contains_key(&channel)
+            map.contains_key(&guild)
         };
         if !game_exists {
             let discord_output = ctx
@@ -29,20 +37,12 @@ impl Pool {
                 .cloned()
                 .expect("Expected DiscordOutput in ShareMap.");
 
-            let guild = ctx
-                .cache
-                .read()
-                .guild_channel(channel)
-                .context("Server not found")?
-                .read()
-                .guild_id;
-
             let dispatcher = OutputPipe::new(guild, channel, &discord_output);
             let mut map = self.games.write().expect("Pool RwLock poisoned");
-            map.insert(channel, Arc::new(Mutex::new(Game::new(dispatcher))));
+            map.insert(guild, Arc::new(Mutex::new(Game::new(dispatcher))));
         }
         let map = self.games.read().expect("Pool RwLock poisoned");
-        Ok(Arc::clone(map.get(&channel).expect("Pool missing game")))
+        Ok(Arc::clone(map.get(&guild).expect("Pool missing game")))
     }
 
     pub fn tick(&self, dt: Duration) {
