560ef66bc16dac375c853fcde511cdc834c452ce
Antoine Gersant <antoine.gersant@lesforges.org>
2020-06-10 16:03:23 -0700

    New players can now join a team mid-game

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -92,34 +92,38 @@ impl Game {
     }
 
     pub fn join_team(&mut self, player: UserId, team_name: &str) -> Result<()> {
-        match &mut self.current_phase {
-            Phase::Setup => {
-                let mut teams = self.teams.write();
-
-                // Remove player from existing team
-                for team in teams.iter_mut() {
-                    team.players.remove(&player);
-                }
-
-                // Put player on his desired team
-                let team_id = TeamId::TeamName(team_name.into());
-                let mut team = teams.iter_mut().find(|team| team.id == team_id);
-                if team.is_none() {
-                    let new_team = Team::new(team_id);
-                    teams.push(new_team);
-                    team = Some(teams.iter_mut().last().expect("Team not found"));
-                }
-                if let Some(team) = team {
-                    team.players.insert(player);
-                }
-
-                // Remove empty teams
-                teams.retain(|t| !t.players.is_empty());
-
-                Ok(())
-            }
-            _ => Err(anyhow!("Cannot join a team outside of setup phase")),
+        let is_setup_phase = match &self.current_phase {
+            Phase::Setup => true,
+            _ => false,
+        };
+        let is_player_on_team = self.get_player_team(player).is_some();
+        if is_player_on_team && !is_setup_phase {
+            return Err(anyhow!("Team can not be changed during a quizz"));
         }
+
+        let mut teams = self.teams.write();
+
+        // Remove player from existing team
+        for team in teams.iter_mut() {
+            team.players.remove(&player);
+        }
+
+        // Put player on his desired team
+        let team_id = TeamId::TeamName(team_name.into());
+        let mut team = teams.iter_mut().find(|team| team.id == team_id);
+        if team.is_none() {
+            let new_team = Team::new(team_id);
+            teams.push(new_team);
+            team = Some(teams.iter_mut().last().expect("Team not found"));
+        }
+        if let Some(team) = team {
+            team.players.insert(player);
+        }
+
+        // Remove empty teams
+        teams.retain(|t| !t.players.is_empty());
+
+        Ok(())
     }
 
     fn get_player_team(&self, player: UserId) -> Option<TeamId> {
