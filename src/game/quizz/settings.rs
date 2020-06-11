6c18cde6d011ec891f1802d8626bff6765a76580
Antoine Gersant <antoine.gersant@lesforges.org>
2020-06-10 17:03:02 -0700

    Alpha settings

--- src/game/quizz/settings.rs
+++ src/game/quizz/settings.rs
@@ -11,10 +11,10 @@ pub struct Settings {
 impl Default for Settings {
     fn default() -> Self {
         Settings {
-            cooldown_duration: Duration::from_secs(15),
-            vote_duration: Duration::from_secs(30),
+            cooldown_duration: Duration::from_secs(5),
+            vote_duration: Duration::from_secs(15),
             question_duration: Duration::from_secs(90),
-            max_vote_options: 5,
+            max_vote_options: 6,
         }
     }
 }
