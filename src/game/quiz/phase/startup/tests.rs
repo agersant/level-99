ff86e26f53279c5746aed2636bdc35e8c341d52a
Antoine Gersant <antoine.gersant@lesforges.org>
2020-08-09 18:44:41 -0700

    Fixed startup test

--- src/game/quiz/phase/startup/tests.rs
+++ src/game/quiz/phase/startup/tests.rs
@@ -1,5 +1,5 @@
 use super::*;
-use std::time::Duration;
+use std::time::{Duration, Instant};
 
 use crate::output::mock::MockGameOutput;
 
@@ -13,8 +13,19 @@ fn ends_after_duration() {
     assert!(!state.is_over());
     state.on_tick(Duration::from_secs(5));
     assert!(!state.is_over());
-    state.on_tick(Duration::from_secs(5));
-    assert!(state.is_over());
+
+    let start_time = Instant::now();
+    let tick_duration = Duration::from_millis(100);
+    loop {
+        state.on_tick(tick_duration);
+        if state.is_over() {
+            break;
+        }
+        if Instant::now().duration_since(start_time) > Duration::from_secs(5) {
+            panic!("Timed out waiting for startup phase to end");
+        }
+        std::thread::sleep(tick_duration);
+    }
 }
 
 #[test]
